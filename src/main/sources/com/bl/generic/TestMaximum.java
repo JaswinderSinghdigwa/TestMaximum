41b74b996d0bc46ffcf2b8afd684bcd8d90b3f5f
Aman <you@example.com>
2021-09-27 18:41:40 +0530

    Refactor

--- src/main/sources/com/bl/generic/TestMaximum.java
+++ src/main/sources/com/bl/generic/TestMaximum.java
@@ -1,9 +1,9 @@
 package com.bl.generic;
 
-public class TestMaximum {
-	public static String maxDouble(String[] x ) {
-		String max =x[0];
-		for (String y : x) {
+public class TestMaximum<E> {
+	public static <E extends Comparable<E>> E max(E[] x ) {
+		E max =x[0];
+		for (E y : x) {
 			if(y.compareTo(max)>0) {
 				max=y;		
 		}
--- src/main/sources/com/bl/main/A.java
+++ src/main/sources/com/bl/main/A.java
@@ -6,7 +6,9 @@ public class A {
 
 	public static void main(String[] args) {
 		String y[] = {"abc","def","Apple"};
+		Integer x[] = {1,2,3};
+		Double z[]= {1.2,2.3,3.4};
 		System.out.println("Welcome To Generic");
-		System.out.print("The maximum value between three integer is:" + TestMaximum.maxString(y));
+		System.out.print("The maximum value between three integer is:" + TestMaximum.max(y));
 	}
 }
