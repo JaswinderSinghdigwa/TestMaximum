ae1c22b39ac4b3caf91d919642af91b3b106855d
Aman <you@example.com>
2021-09-27 19:33:06 +0530

    Refactor2

--- src/main/sources/com/bl/generic/TestMaximum.java
+++ src/main/sources/com/bl/generic/TestMaximum.java
@@ -1,20 +1,32 @@
 package com.bl.generic;
 
 public class TestMaximum<E> {
-	private E[] x,y,z;
-	public static <E extends Comparable<E>> E max(E...x) {
-		E max =x[0];
-		for (E y1 : x) {
-			if(y1.compareTo(max)>0) {
-				max=y1;	
-//				System.out.print("Maximum value is :" + max);
-		}
-			//Third no maximum
-//		if(y2[2]==max) {
-//			System.out.println("third element is maximum no");
-//		}
-			
+	private E firstvalue;
+	private E secndvalue;
+	private E thirdvalue;
+//	private E[] extraparameter;
+	public TestMaximum(E firstvalue, E secndvalue, E thirdvalue) {
+		super();
+		this.firstvalue = firstvalue;
+		this.secndvalue = secndvalue;
+		this.thirdvalue = thirdvalue;
+//		this.extraparameter = extraparameter;
 	}
+	public static <E extends Comparable<E>> E max(E firstvalue ,E secondvalue,E thirdvalue ) {
+		E max = firstvalue;
+			if(secondvalue.compareTo(max)>0) {
+				max = secondvalue;	
+			}
+			if(thirdvalue.compareTo(max)>0) {
+				max = thirdvalue;	
+			}
+//			if(extraparameter.length != 0) {
+//				for(E Y : extraparameter) {
+//					if(Y.compareTo(max)>0) {
+//						max=Y;
+//					}
+//				}
+//			}
 	return max;
 	}
 }
--- src/main/sources/com/bl/main/A.java
+++ src/main/sources/com/bl/main/A.java
@@ -6,8 +6,8 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Generic");
-		System.out.print("The maximum value between three integer is:" + TestMaximum.max(Integer.valueOf(78),Integer.valueOf(45),Integer.valueOf(67)));
-		System.out.print("The Maximum value between three string is:" + TestMaximum.max(Double.valueOf(78.9),Double.valueOf(74.9),Double.valueOf(75.9)));
-		System.out.print("The Maximum value between three string is:" + TestMaximum.max(String.valueOf("abc"),String.valueOf("rose"),String.valueOf("Apple")));
+		System.out.println("The maximum value between three integer is:" + TestMaximum.max(78,45,67));
+		System.out.println("The Maximum value between three string is:" + TestMaximum.max(78.9,74.9,75.9));
+		System.out.println("The Maximum value between three string is:" + TestMaximum.max("abc","rose","Apple"));
 	}
 }
